cc09b5c2cd9b17b22012c4631d3a67c7ae918b4f
Delowar Hossain <bscdelowar@yahoo.com>
2023-02-18 09:11:25 +0600

    2 functions added

--- js/functions.js
+++ js/functions.js
@@ -1,20 +1,35 @@
+//Function for area calculation by shape id. 
+function areaCalculationById(shapeId, input1, input2){
+    const shapeName=document.getElementById(shapeId).innerText;
+    const b=getInputFieldValue(input1);
+    const h=getInputFieldValue(input2);
+    let area;
+    if(shapeId=='traingle'||shapeId=='rhombus'||shapeId=='pentagon'){
+        area=0.5*b*h;
+    }else{
+        area=b*h;
+    }
+    //show result
+    if(!isNaN(area)){
+        removeError();
+        dispalyResult(shapeName, area);
+    } 
+}
+
 //Function to Get Input Field value as integer number 
     function getInputFieldValue(id){
         const getInputString=document.getElementById(id).value;
-        const getNumber=parseInt(getInputString);
-        return getNumber;
+        const getNumber=parseFloat(getInputString);
+        document.getElementById(id).value='';
+        if(isNaN(getNumber)){
+            const msg=`<strong>${id}</strong> can not be empty or string`;
+            setError('error', msg);
+        }else if(getNumber<0){
+            const msg=`<strong>${id}</strong> must be positive number`;
+            setError('error', msg);
+        }
+        else{
+            return getNumber;
+        }  
     }
 
-// Function to Dispaly result 
-    let i=0;
-    function dispalyResult(itemName, area){
-        i+=1;
-        const tBody=document.getElementById('table-body');
-        const tr=document.createElement('tr');
-        tr.innerHTML=`
-        <td>${i}</td>
-        <td>${itemName}</td>
-        <td>${area}</td>
-        `;
-        tBody.appendChild(tr);
-    }
--- js/js.js
+++ js/js.js
@@ -1,8 +1,31 @@
 //Traingle Area
 document.getElementById('btn-traingle').addEventListener('click', function(){
-    const traingle=document.getElementById('traingle').innerText;
-    const b=getInputFieldValue('input-b');
-    const h=getInputFieldValue('input-h');
-    const traingleArea=0.5*b*h;
-    dispalyResult(traingle, traingleArea);
+    const traingleArea=areaCalculationById('traingle', 'input-b', 'input-h');  
+})
+//Rectangle Area
+document.getElementById('btn-rectangle').addEventListener('click', function(){
+    const rectangleArea=areaCalculationById('rectangle', 'input-w', 'input-i');
+})
+//Parallelogram Area
+document.getElementById('btn-parallelogram').addEventListener('click', function(){
+    const parallelogramArea=areaCalculationById('parallelogram', 'parallelogram-b', 'parallelogram-h');
+})
+//Rhombus Area
+document.getElementById('btn-rhombus').addEventListener('click', function(){
+    const rhombusArea=areaCalculationById('rhombus', 'input-d1', 'input-d2');
+})
+//Pentagon Area
+document.getElementById('btn-pentagon').addEventListener('click', function(){
+    const pentagonArea=areaCalculationById('pentagon', 'pentagon-input-p', 'pentagon-input-b');
+})
+//Ellips Area
+document.getElementById('btn-ellipse').addEventListener('click', function(){
+    const ellipse=document.getElementById('ellipse').innerText;
+    const a=getInputFieldValue('ellipse-input-a');
+    const b=getInputFieldValue('ellipse-input-b');
+    const ellipsArea=3.14*a*b;
+    if(!isNaN(ellipsArea)){
+        removeError();
+        dispalyResult(ellipse, ellipsArea);
+    } 
 })
